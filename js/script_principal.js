e51349e4ddfd19a7a9a11be75ae2122a16964b8a
Adahirsito117 <adahir.hm@gmail.com>
2026-04-28 20:09:22 -0600

    Inicialización de proyecto

---
+++ js/script_principal.js
@@ -0,0 +1,10 @@
+let usuario = localStorage.getItem("usuario");
+
+document.getElementById("bienvenida").innerHTML = "Bienvenido, " + usuario;
+
+function cerrarSesion(){
+    localStorage.removeItem("usuario");
+    window.location.href = "/index.html";
+}
+
+document.getElementById("btnCerrarSesion").addEventListener("click", cerrarSesion);
